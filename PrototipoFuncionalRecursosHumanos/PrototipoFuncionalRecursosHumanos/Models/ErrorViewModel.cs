6efd8505b8fd69682fbf0877745125fc80947e69
alexabryan02 <alexabr13@hotmail.com>
2024-04-22 23:18:03 -0600

    Se creo el proyecto

---
+++ PrototipoFuncionalRecursosHumanos/PrototipoFuncionalRecursosHumanos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrototipoFuncionalRecursosHumanos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
