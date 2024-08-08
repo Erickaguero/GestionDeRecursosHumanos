3852e7b94267427aee21aaf842652b9f264653b7
alexabryan02 <alexabr13@hotmail.com>
2024-08-08 17:38:21 -0600

    Cambios a las validaciones

--- PrototipoFuncionalRecursosHumanos/PrototipoFuncionalRecursosHumanos/obj/Debug/net8.0/PrototipoFuncionalRecursosHumanos.AssemblyInfo.cs
+++ PrototipoFuncionalRecursosHumanos/PrototipoFuncionalRecursosHumanos/obj/Debug/net8.0/PrototipoFuncionalRecursosHumanos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrototipoFuncionalRecursosHumanos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b493d07dfa73b146829d6300ee3bbfee3b27589a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436d40abf4838ba7cd2e4fb7791d38910132b157")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrototipoFuncionalRecursosHumanos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrototipoFuncionalRecursosHumanos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
